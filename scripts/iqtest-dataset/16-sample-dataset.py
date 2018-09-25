ed4beb2c5bddef57bc9f746dc7532aa5f2efaae8
Florian Golemo <fgolemo@gmail.com>
2018-09-25 18:14:50 -0400

    added sampling script

---
+++ scripts/iqtest-dataset/16-sample-dataset.py
@@ -0,0 +1,37 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from inversegraphics_generator.img_dataset import IqImgDataset
+from inversegraphics_generator.iqtest_objs import get_data_dir
+
+ds_train = IqImgDataset("/data/lisa/data/iqtest/iqtest-dataset.h5", "train/labeled")
+ds_test = IqImgDataset("/data/lisa/data/iqtest/iqtest-dataset.h5", "test")
+
+OUT = os.path.join(get_data_dir(),"sample-{traintest}-{refans}-{idx}.png")
+
+def reshape(img):
+    img = np.swapaxes(img, 1,3)
+    img = np.swapaxes(img, 1,2)
+    img *= 255
+    img = np.clip(img, 0, 255)
+    return img.astype(np.uint8)
+
+def sample(ds, idx, traintest):
+    plt.imsave(OUT.format(traintest, "ref", idx),reshape(ds[idx][0][0]))
+
+    ans_idx = ds[idx][1].argmax()
+    for ans in range(3):
+        label = "ans"+(ans+1)
+        if ans == ans_idx:
+            label += "c"
+        plt.imsave(OUT.format(traintest, label, idx),ds[idx][0][1+ans])
+
+
+for i in range(10):
+    sample(ds_train, i, "train")
+    sample(ds_test, i, "test")
+
+
+
+
+

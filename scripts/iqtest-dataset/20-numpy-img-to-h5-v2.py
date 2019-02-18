4fa4c6ba0d3e6d7f59f08506fb1d7d0ae293944b
Florian Golemo <fgolemo@gmail.com>
2019-02-18 14:29:51 -0500

    smol correction in the file creation process

--- scripts/iqtest-dataset/20-numpy-img-to-h5-v2.py
+++ scripts/iqtest-dataset/20-numpy-img-to-h5-v2.py
@@ -1,13 +1,14 @@
 import numpy as np
 import h5py
 from tqdm import tqdm
+
 # from inversegraphics_generator.iqtest_objs import get_data_dir
 
 # IN_PATH = get_data_dir() + "/{}000_baseline_data.npy"
-IN_PATH = "/network/data1/sai/Florian/train/{}000_baseline_data.npy"
+IN_PATH = "/data/Florian/{}/{}000_baseline_data.npy"
 
 # OUT_PATH = get_data_dir() + "/3diqtt-v2.h5"
-OUT_PATH = "/network/data1/sai/Florian/train/3diqtt-v2.h5"
+OUT_PATH = "/data/3diqtt-v2-{}.h5"
 
 CHUNK = 5000
 
@@ -27,32 +28,55 @@ def prep(a):
     return a
 
 
+with h5py.File(OUT_PATH.format("test"), "w") as f:
+    test_q = f.create_dataset("questions", (10000, 4, 128, 128, 3), dtype=np.float32)
 
-with h5py.File(OUT_PATH, "w") as f:
-    train = f.create_group("train")
-    test = f.create_group("test")
-    val = f.create_group("val")
+    for i in tqdm(range(2)):
+        data = np.load(IN_PATH.format("test", (i + 1) * 5), mmap_mode="c")
+        data = prep(data)
 
-    train_labeled_group = train.create_group("labeled")
-    train_unlabeled_group = train.create_group("unlabeled")
+        col_a = data[:, 0, :, :, :]
+        col_b = np.roll(data[:, 1, :, :, :], 1234, axis=0)
+        col_c = np.roll(data[:, 2, :, :, :], 2468, axis=0)
+        col_d = data[:, 3, :, :, :]  # copy of col_a
 
-    labeled_q = train_labeled_group.create_dataset("questions", (10000, 4, 128, 128, 3),
-                                                   dtype=np.float32,
-                                                   compression="gzip")
-    labeled_a = train_labeled_group.create_dataset("answers", (10000,), dtype=np.uint8, compression="gzip")
+        cols = [col_b, col_c, col_d]
 
-    unlabeled_q = train_unlabeled_group.create_dataset("questions", (90000, 4, 128, 128, 3), dtype=np.float32,
-                                                       compression="gzip")
+        questions = np.zeros((CHUNK, 4, 128, 128, 3), dtype=np.float32)
+        answers = np.zeros((CHUNK), dtype=np.uint8)
 
-    test_q = test.create_dataset("questions", (10000, 4, 128, 128, 3), dtype=np.float32, compression="gzip")
+        order = np.array([0, 1, 2])
 
-    val_q = val.create_dataset("questions", (10000, 4, 128, 128, 3), dtype=np.float32, compression="gzip")
-    val_a = val.create_dataset("answers", (10000,), dtype=np.uint8, compression="gzip")
+        for idx in range(CHUNK):
+            questions[idx, 0] = col_a[idx]
+            np.random.shuffle(order)
+            answers[idx] = np.where(order == 2)[0]
+            questions[idx, 1] = cols[order[0]][idx]
+            questions[idx, 2] = cols[order[1]][idx]
+            questions[idx, 3] = cols[order[2]][idx]
 
-    ### TRAIN
+        indices = np.arange(0, CHUNK)
+        np.random.shuffle(indices)
 
-    for i in range(18):
-        data = np.load(IN_PATH.format((i + 1) * 5), mmap_mode="c")
+        # mem-efficient shuffle in place
+        rng = np.random.get_state()
+        np.random.shuffle(questions)
+        np.random.set_state(rng)
+        np.random.shuffle(answers)
+
+        test_q[(i * 5000):((i + 1) * 5000)] = questions
+        # labeled_a[(i * 5000):((i + 1) * 5000)] = answers
+
+        del data
+
+print("test done, doing validation now")
+
+with h5py.File(OUT_PATH.format("val"), "w") as f:
+    val_q = f.create_dataset("questions", (10000, 4, 128, 128, 3), dtype=np.float32)
+    val_a = f.create_dataset("answers", (10000,), dtype=np.uint8)
+
+    for i in tqdm(range(2)):
+        data = np.load(IN_PATH.format("train", (i + 21) * 5), mmap_mode="c")
         data = prep(data)
 
         col_a = data[:, 0, :, :, :]
@@ -67,7 +91,58 @@ with h5py.File(OUT_PATH, "w") as f:
 
         order = np.array([0, 1, 2])
 
-        for idx in tqdm(range(CHUNK)):
+        for idx in range(CHUNK):
+            questions[idx, 0] = col_a[idx]
+            np.random.shuffle(order)
+            answers[idx] = np.where(order == 2)[0]
+            questions[idx, 1] = cols[order[0]][idx]
+            questions[idx, 2] = cols[order[1]][idx]
+            questions[idx, 3] = cols[order[2]][idx]
+
+        indices = np.arange(0, CHUNK)
+        np.random.shuffle(indices)
+
+        # mem-efficient shuffle in place
+        rng = np.random.get_state()
+        np.random.shuffle(questions)
+        np.random.set_state(rng)
+        np.random.shuffle(answers)
+
+        val_q[(i * 5000):((i + 1) * 5000)] = questions
+        val_a[(i * 5000):((i + 1) * 5000)] = answers
+
+        del data
+
+print("val done, doing train now")
+
+with h5py.File(OUT_PATH.format("train"), "w") as f:
+    train_labeled_group = f.create_group("labeled")
+    train_unlabeled_group = f.create_group("unlabeled")
+
+    labeled_q = train_labeled_group.create_dataset("questions", (10000, 4, 128, 128, 3),
+                                                   dtype=np.float32)
+    labeled_a = train_labeled_group.create_dataset("answers", (10000,), dtype=np.uint8)
+    unlabeled_q = train_unlabeled_group.create_dataset("questions", (90000, 4, 128, 128, 3), dtype=np.float32)
+
+    ### TRAIN
+
+    for i in tqdm(range(20)):
+        data = np.load(IN_PATH.format("train", (i + 1) * 5), mmap_mode="c")
+        data = prep(data)
+
+        col_a = data[:, 0, :, :, :]
+        col_b = np.roll(data[:, 1, :, :, :], 2345, axis=0)
+        col_c = np.roll(data[:, 2, :, :, :], 666, axis=0)
+        col_d = data[:, 3, :, :, :]  # copy of col_a
+
+        cols = [col_b, col_c, col_d]
+
+        questions = np.zeros((CHUNK, 4, 128, 128, 3), dtype=np.float32)
+        answers = np.zeros((CHUNK), dtype=np.uint8)
+
+        order = np.array([0, 1, 2])
+
+        for idx in range(CHUNK):
             questions[idx, 0] = col_a[idx]
             np.random.shuffle(order)
             answers[idx] = np.where(order == 2)[0]
@@ -87,43 +162,4 @@ with h5py.File(OUT_PATH, "w") as f:
         labeled_q[(i * 5000):((i + 1) * 5000)] = questions
         labeled_a[(i * 5000):((i + 1) * 5000)] = answers
 
-        print ("slice done")
-
         del data
-
-
-    # for i in range(18):
-    #     data = load(IN_PATH.format((i + 1) * 5))
-    #
-    #     col_a = data[:, 0, :, :, :]
-    #     col_b = np.roll(data[:, 1, :, :, :], 2345, axis=0)
-    #     col_c = np.roll(data[:, 2, :, :, :], 666, axis=0)
-    #     col_d = data[:, 3, :, :, :]  # copy of col_a
-    #
-    #     cols = [col_b, col_c, col_d]
-    #
-    #     questions = np.zeros((CHUNK, 4, 128, 128, 3), dtype=np.float32)
-    #     answers = np.zeros((CHUNK), dtype=np.uint8)
-    #
-    #     order = np.array([0, 1, 2])
-    #
-    #     for idx in tqdm(range(CHUNK)):
-    #         questions[idx, 0] = col_a[idx]
-    #         np.random.shuffle(order)
-    #         answers[idx] = np.where(order == 2)[0]
-    #         questions[idx, 1] = cols[order[0]][idx]
-    #         questions[idx, 2] = cols[order[1]][idx]
-    #         questions[idx, 3] = cols[order[2]][idx]
-    #
-    #     indices = np.arange(0, CHUNK)
-    #     np.random.shuffle(indices)
-    #
-    #     # mem-efficient shuffle in place
-    #     rng = np.random.get_state()
-    #     np.random.shuffle(questions)
-    #     np.random.set_state(rng)
-    #     np.random.shuffle(answers)
-    #
-    #     labeled_q[(i * 5000):((i + 1) * 5000)] = questions
-    #     labeled_a[(i * 5000):((i + 1) * 5000)] = answers
-
--- scripts/iqtest-dataset/21-test-h5-v2.py
+++ scripts/iqtest-dataset/21-test-h5-v2.py
@@ -1,15 +1,49 @@
+import random
+import time
+
 import numpy as np
 import h5py
 from tqdm import tqdm
 from inversegraphics_generator.iqtest_objs import get_data_dir
 
-OUT_PATH = get_data_dir() + "/3diqtt-v2-x.h5"
+OUT_PATH = get_data_dir() + "/3diqtt-v2-x2.h5"
 
 f = h5py.File(OUT_PATH, 'r')
 
-print (list(f.keys()))
+print(list(f.keys()))
 
+q = f["train/labeled/questions"]
 
-# dset = f['mydataset']
+print(q.shape)
+print(f["train/labeled/answers"].shape)
+print(f["train/unlabeled/questions"].shape)
 
+print(f["test/questions"].shape)
 
+print(f["val/questions"].shape)
+print(f["val/answers"].shape)
+
+import matplotlib.pyplot as plt
+idx = 0
+
+for i in range(3):
+    start = time.time()
+    idx = random.randrange(0,len(q))
+    x, axarr = plt.subplots(1,4, sharex=True, figsize=(20, 10))
+    for j in range(4):
+        axarr[j].imshow(q[idx,j])
+    print(q[idx])
+    diff = time.time()-start
+    print ("time: ",np.around(diff,4))
+    plt.show()
+    # idx += 1
+
+## WITH gzip
+# file size: 2GB per 10k data
+# random access: ~8s per 4 img
+# sequential access: ~6s per 4 img
+
+## WITHOUT gzip
+# file size: 2.4GB per 10k data
+# random access: ~0.2s per 4 img
+# sequential access: ~0.2s per 4 img
